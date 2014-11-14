19400b269fd62626fc5fb5066890980340122fc9
zcfrank1st <zcfrank1st@sina.com>
2014-11-14 13:38:02 +0800

    project init

---
+++ example/PingppExample.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by zcfrank1st on 11/14/14.
+ */
+object PingppExample {
+
+}
---
+++ src/main/scala/com/chaos/pingplusplus/Pingpp.scala
@@ -0,0 +1,8 @@
+package com.chaos.pingplusplus
+
+/**
+ * Created by zcfrank1st on 11/14/14.
+ */
+class Pingpp {
+
+}
